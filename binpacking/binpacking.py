1c120e3bf0701ae83601d3d2797404293a99f9fe
vahidbe <vahid.beyraghi@student.uclouvain.be>
2019-12-01 23:10:52 +0100

    random_walk

--- binpacking/binpacking.py
+++ binpacking/binpacking.py
@@ -3,18 +3,62 @@
 from search import *
 import sys
 
+successorDict = {}
+fitnessDict = {}
 
 class BinPacking(Problem):
 
     def successor(self, state):
-        pass
+        key = hash(state)
+        value = successorDict.get(key, None)
+        if(value!=None):
+            return value
+
+        action_statelist = []
+        #print(state.bins)
+
+        for i in range(state.bins.__len__()):
+            #print("keys: "+str(state.bins[i].keys()))
+            for j in state.bins[i].keys():
+                for k in range(state.bins.__len__()):
+                    if state.can_fit(state.bins[k], state.bins[i].get(j, None)):
+                        #print("move object "+str(j)+" from "+str(i)+" to "+str(k))
+                        action = "move object "+str(j)+" from "+str(i)+" to "+str(k)
+                        newstate = state.copy()
+                        newstate.move(i,j,k)
+                        action_statelist.append((action, newstate))
+
+        result = action_statelist
+        successorDict[key] = result
+        return result
+
+    def value(self, state):
+        return self.fitness(state)
 
     def fitness(self, state):
         """
         :param state:
         :return: fitness value of the state in parameter
         """
-        return 0
+        key = hash(state)
+        value = fitnessDict.get(key, None)
+        if(value!=None):
+            return value
+
+        sum = 0
+        for i in range(state.bins.__len__()):
+            sum = sum + math.pow(self.fullness(state, i)/state.capacity,2)
+
+        result = 1 - sum/state.bins.__len__()
+
+        fitnessDict[key] = result
+        return result
+    
+    def fullness(self, state, i):
+        sum = 0
+        for ind,size in state.bins[i].items():
+            sum = sum + size
+        return sum
 
 
 class State:
@@ -41,8 +85,25 @@ class State:
     def __str__(self):
         s = ''
         for i in range(len(self.bins)):
-            s += ' '.join(list(self.bins[i].keys())) + '\n'
+            s += ' '.join(str(list(self.bins[i].keys()))) + '\n'
         return s
+    
+    def copy(self):
+        newstate = State(self.capacity, self.items.copy())
+        newstate.bins.clear()
+        for b in self.bins:
+            newstate.bins.append(b.copy())
+        return newstate
+
+    def move(self, i, j, k):
+        item = self.bins[i].pop(j, None)
+        if (item == None):
+            print(j)
+            print(self.bins[i].keys())
+            print("ERROR NONE")
+        self.bins[k][j] = item
+
+
 
 
 def read_instance(instanceFile):
@@ -73,6 +134,9 @@ def randomized_maxvalue(problem, limit=100, callback=None):
 
     return best
 
+def hash(state):
+    return state.__str__()
+
 
 #####################
 #       Launch      #
@@ -82,6 +146,7 @@ if __name__ == '__main__':
     init_state = State(info[0], info[1])
     bp_problem = BinPacking(init_state)
     step_limit = 100
-    node = randomized_maxvalue(bp_problem, step_limit)
+    #node = randomized_maxvalue(bp_problem, step_limit)
+    node = random_walk(bp_problem, step_limit)
     state = node.state
     print(state)
